7dfd65a1b811c76e7738ee70f40b3dad9e839d17
Ryusei Okuda <clz14026@ict.nitech.ac.jp>
2023-09-17 23:07:21 +0900

    Section3.7Finish

---
+++ django_blog/blogs/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render , get_object_or_404
+from .models import Blog
+
+# Create your views here.
+def index(request):
+    blogs = Blog.objects.order_by('-created_datetime')
+    return render(request,'blogs/index.html',{'blogs': blogs})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, id= blog_id)
+    return render(request,'blogs/detail.html',{'blog': blog})
