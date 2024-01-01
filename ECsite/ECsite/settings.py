d9187ad50e75340d5350dd48e75b9e4c6bc8319d
Ryusei Okuda <clz14026@ict.nitech.ac.jp>
2024-01-01 23:52:08 +0900

    Section5.2Finish

--- ECsite/ECsite/settings.py
+++ ECsite/ECsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'users',
+    'app',
 ]
 
 AUTH_USER_MODEL = 'users.User'
@@ -118,3 +119,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# ログイン関連
+LOGIN_URL = 'app:login'
+LOGIN_REDIRECT_URL = 'app:index'
+LOGOUT_REDIRECT_URL = 'app:index'
